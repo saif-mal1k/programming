dfe2f6d1b63ba1025f7c1b7a188f2000656914ab
saif_mal1k <saifmalik1200@gmail.com>
2021-10-29 17:30:17 +0530

    added new file

---
+++ C Programming/examples/new.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int a,b;
+	printf("enter value for a,b:");
+	scanf("%d,%d",&a,&b);
+	
+	printf("sum is:%d",a+b);
+	return 0;
+}
---
+++ c++/new.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"what are you doing";
+	return 0;
+}
